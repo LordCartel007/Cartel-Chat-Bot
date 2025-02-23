d009257caa2873562f8a554d889a38d817cb995e
LordCartel007 <cartellord77@gmail.com>
2025-02-23 18:34:00 +0100

    added stop response button

--- script.js
+++ script.js
@@ -3,15 +3,18 @@ const chatsContainer = document.querySelector(".chats-container");
 const promptForm = document.querySelector(".prompt-form");
 const promptInput = promptForm.querySelector(".prompt-input");
 const fileInput = promptForm.querySelector("#file-input");
+const fileUploadWrapper = promptForm.querySelector(".file-upload-wrapper");
+const themeToggle = document.querySelector("#theme-toggle-btn");
 
 // API KEY SETUP
 const API_KEY = "AIzaSyDv92d7AZgqgMMdutLtx9FL2tiBFhbfJ-E";
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
 
-let userMessage = "";
-
+let typingInterval, controller;
 // storing every bot and user conversation so the bot can know the chat history
 const chatHistory = [];
+
+const userData = { message: "", file: {} };
 
 // function to create a message element with the given content and classes
 const createMsgElement = (content, ...classes) => {
@@ -32,30 +35,44 @@ const typingEffect = (text, textElement, botMsgDiv) => {
   let wordIndex = 0;
 
   // set an interval to type each word
-  const typingInterval = setInterval(() => {
+  typingInterval = setInterval(() => {
     if (wordIndex < words.length) {
       textElement.textContent +=
         (wordIndex === 0 ? "" : " ") + words[wordIndex++];
-      botMsgDiv.classList.remove("loading");
       scrollToBottom();
     } else {
       clearInterval(typingInterval);
+      botMsgDiv.classList.remove("loading");
+      document.body.classList.remove("bot-responding");
     }
   }, 40);
 };
 // Make the api call and generate the bot response
 const generateResponse = async (botMsgDiv) => {
   const textElement = botMsgDiv.querySelector(".message-text");
-  // Add user message to chat history
+  controller = new AbortController();
+  // Add user message and file data to chat history
   chatHistory.push({
     role: "user",
-    parts: [{ text: userMessage }],
+    parts: [
+      { text: userData.message },
+      ...(userData.file.data
+        ? [
+            {
+              inline_data: (({ fileName, isImage, ...rest }) => rest)(
+                userData.file
+              ),
+            },
+          ]
+        : []),
+    ],
   });
   try {
     const response = await fetch(API_URL, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ contents: chatHistory }),
+      signal: controller.signal,
     });
     const data = await response.json();
     if (!response.ok) throw new Error(data.error.message);
@@ -65,23 +82,52 @@ const generateResponse = async (botMsgDiv) => {
       .replace(/\*\*([^*]+)\*\*/g, "$1")
       .trim();
     typingEffect(responseText, textElement, botMsgDiv);
+    // adding the models response to the chat history for better interactions
+    chatHistory.push({
+      role: "model",
+      parts: [{ text: responseText }],
+    });
+    console.log(chatHistory);
   } catch (error) {
-    console.log(error);
+    textElement.style.color = "#d62939";
+    textElement.textContent =
+      error.name === "AbortError"
+        ? "Request generation stopped"
+        : error.message;
+    botMsgDiv.classList.remove("loading");
+    document.body.classList.remove("bot-responding");
+  } finally {
+    userData.file = {};
+    scrollToBottom();
   }
 };
 
 //Handle the form submission
 const handleFormSubmit = (e) => {
   e.preventDefault();
-  userMessage = promptInput.value.trim();
-
-  if (!userMessage) return;
+  const userMessage = promptInput.value.trim();
+  // if the bot is still responding restrict the user from sending a new message.
+  if (!userMessage || document.body.classList.contains("bot-responding"))
+    return;
 
   //   clearing prompt input value
   promptInput.value = "";
+  userData.message = userMessage;
+  document.body.classList.add("bot-responding", "chats-active");
+  fileUploadWrapper.classList.remove("active", "img-attached", "file-attached");
 
   //   Generate user message HTML and add it to the chats containers
-  const userMsgHTML = `<p class="message-text"></p>`;
+  // adding the attachment message weather its an image or a file
+  const userMsgHTML = `<p class="message-text"></p>
+  ${
+    userData.file.data
+      ? userData.file.isImage
+        ? `<img src="data:${userData.file.mime_type};base64,
+    ${userData.file.data}" class="img-attachment" />`
+        : `<p class="file-attachment"><span class="material-symbols-rounded"> 
+    description</span>${userData.file.fileName}</p>`
+      : ""
+  }`;
   const userMsgDiv = createMsgElement(userMsgHTML, "user-message");
   userMsgDiv.querySelector(".message-text").textContent = userMessage;
   chatsContainer.appendChild(userMsgDiv);
@@ -97,6 +143,89 @@ const handleFormSubmit = (e) => {
     generateResponse(botMsgDiv);
   }, 600);
 };
+
+// Handle file input change (file upload)
+fileInput.addEventListener("change", () => {
+  const file = fileInput.files[0];
+  if (!file) return;
+
+  const isImage = file.type.startsWith("image/");
+  const reader = new FileReader();
+  reader.readAsDataURL(file);
+
+  // clearing the input file value
+  reader.onload = (e) => {
+    fileInput.value = "";
+    // germini only receives the base64 string of the file
+    const base64String = e.target.result.split(",")[1];
+    fileUploadWrapper.querySelector(".file-preview").src = e.target.result;
+    fileUploadWrapper.classList.add(
+      "active",
+      isImage ? "img-attached" : "file-attached"
+    );
+
+    // Store file data in userData obj
+    userData.file = {
+      fileName: file.name,
+      data: base64String,
+      mime_type: file.type,
+      isImage,
+    };
+  };
+});
+
+// Handle file upload cancel
+document.querySelector("#cancel-file-btn").addEventListener("click", () => {
+  userData.file = {};
+  fileUploadWrapper.classList.remove("active", "img-attached", "file-attached");
+});
+
+// stop ongoing bot response
+document.querySelector("#stop-response-btn").addEventListener("click", () => {
+  userData.file = {};
+  controller?.abort();
+  clearInterval(typingInterval);
+  chatsContainer
+    .querySelector(".bot-message.loading")
+    .classList.remove("loading");
+  document.body.classList.remove("bot-responding");
+});
+
+// Delete all chats
+document.querySelector("#delete-chats-btn").addEventListener("click", () => {
+  chatHistory.length = 0;
+  chatsContainer.innerHTML = "";
+  document.body.classList.remove("bot-responding", "chats-active");
+});
+
+// Handle suggestions click to add to prompt input
+document.querySelectorAll(".suggestions-item").forEach((item) => {
+  item.addEventListener("click", () => {
+    promptInput.value = item.querySelector(".text").textContent;
+    promptForm.dispatchEvent(new Event("submit"));
+  });
+});
+
+// show/hide controls for mobile on prompt input focus
+document.addEventListener("click", ({ target }) => {
+  const wrapper = document.querySelector(".prompt-wrapper");
+  const shouldHide =
+    target.classList.contains("prompt-input") ||
+    (wrapper.classList.contains("hide-controls") &&
+      (target.id === "add-file-btn" || target.id === "stop-response-btn"));
+  wrapper.classList.toggle("hide-controls", shouldHide);
+});
+
+// toggle theme dark/light and save in local storage for persistence
+themeToggle.addEventListener("click", () => {
+  const isLightTheme = document.body.classList.toggle("light-theme");
+  localStorage.setItem("themeColor", isLightTheme ? "light_mode" : "dark_mode");
+  themeToggle.textContent = isLightTheme ? "dark_mode" : "light_mode";
+});
+//set initial theme from local storage
+const isLightTheme = localStorage.getItem("themeColor") === "light_mode";
+document.body.classList.toggle("light-theme", isLightTheme);
+themeToggle.textContent = isLightTheme ? "dark_mode" : "light_mode";
 
 promptForm.addEventListener("submit", handleFormSubmit);
 promptForm
